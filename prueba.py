4c0cf52516f7ffa262ae94c2cda49d1d934d7963
Diego-Lasluisa1 <diego.lasluisa@utc.edu.ec>
2025-05-13 12:57:43 -0500

    issues#1

---
+++ prueba.py
@@ -0,0 +1,11 @@
+import datetime as date;
+
+dato = date.datetime.now().year
+
+
+# print(dato)
+
+fecha = int(input("EDAD: "))
+print("edad  ", fecha)
+print("fecha actual ", dato)
+print("Tu año de nacimiento: ", dato-fecha)
